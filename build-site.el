3f2fe927e2868a72c911a0ddfa5679cb951c9a93
Anon Emoose <anon.33.m00se@lamedomainname.com>
2024-11-28 10:22:10 -0500

    Cheese

--- build-site.el
+++ build-site.el
@@ -28,13 +28,14 @@
       org-html-doctype                 "html5"
       org-html-html5-fancy                   t
       org-html-head-include-default-style  nil ;; skip the default style sheet
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>")
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"customizations.css\" />\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=0.25\">")
 
 (setq org-publish-project-alist
   (list
    (list "stuff"
      :recursive t
      :base-directory "./content"
+     :base-extension "org"
      :publishing-directory "./public"
      :publishing-function 'org-html-publish-to-html
      :with-author nil
@@ -43,7 +44,17 @@
      :with-toc nil
      :section-numbers nil
      :time-stamp-file t)))
-;;     :body-only t)))
+;;   (list "static"
+;;     :recursive t
+;;     :base-directory "./static"
+;;     :publishing-directory "./public"
+;;     :publishing-function 'org-html-publish-to-html
+;;     :with-author nil
+;;     :with-title nil
+;;     :with-creator t
+;;     :with-toc nil
+;;     :section-numbers nil
+;;     :time-stamp-file t)))
 
 (org-publish-all t)
      
